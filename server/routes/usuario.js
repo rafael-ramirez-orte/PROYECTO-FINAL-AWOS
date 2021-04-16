8bf78bda81a5622494884e5d794759c7fdfa1056
rafael.ramirez <rafa.rami12.10@gmail.com>
2021-04-15 19:00:13 -0500

    Proyecto listo

---
+++ server/routes/usuario.js
@@ -0,0 +1,119 @@
+const { json } = require('body-parser');
+const { request, query } = require('express');
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
+const app = express();
+
+app.get('/usuario', function(req, res) {
+    let desde = req.query.desde || 0;
+    let hasta = req.query.hasta || 5;
+
+    Usuario.find({ estado: true })
+        .skip(Number(desde))
+        .limit(Number(hasta))
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400), json({
+                    ok: false,
+                    msg: 'Ocurrio un error al momento de consultar',
+                    err
+                });
+            }
+            res.json({
+                ok: true,
+                msg: 'Lista de usuarios obtenida con exito',
+                conteo: usuarios.length,
+                usuarios
+            });
+        });
+});
+
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    let usr = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10)
+    });
+
+    usr.save((err, usrDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ocurrio un error',
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            msg: 'Usuario insertado con exito',
+            usrDB
+        });
+    });
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' },
+        (err, usrDB) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Ocurrio un error al momento de actualizar',
+                    err
+                });
+            }
+
+            res.json({
+                ok: true,
+                msg: 'Usuario actualizado con exito',
+                usuario: usrDB
+            });
+        });
+});
+
+app.delete('/Usuario/:id', function(req, res) {
+    // let id = req.params.id;
+
+    //Usuario.deleteOne({ _id: id }, (err, usuarioBorrado) => {
+    //  if (err) {
+    //    return res.status(400).json({
+    //      ok: false,
+    //    msg: 'Ocurrio un error al momento de eliminar',
+    //  err
+    //});
+
+    //}
+
+    //res.json({
+    //  ok: true,
+    //msg: 'Usuario eliminado con exito',
+    //usuarioBorrado
+    //});
+    //});
+
+    let id = req.params.id;
+
+    Usuario.findByIdAndUpdate(id, { estado: false }, { new: true, runValidators: true, context: 'query' }, (err, usrDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Ocurrio un error al momento de eliminar',
+                err
+            });
+
+        }
+
+        res.json({
+            ok: true,
+            msg: 'Usuario eliminado con exito',
+            usrDB
+        });
+    });
+});
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,36 @@
+require('./config/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.send('<h1>Bienvenido a mi servidor REST (localhost)</h1>');
+});
+
+app.use(require('./routes/usuario'));
+app.use(require('./routes/categoria'));
+app.use(require('./routes/login'));
+app.use(require('./routes/producto'));
+
+mongoose.connect('mongodb+srv://admin:Cruzazul1@cluster0.ppjlk.mongodb.net/cafeteria', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log('El servidor esta en linea por el puerto ', process.env.PORT);
+});
